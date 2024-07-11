f7f28974ea5f53046672965b9e6bf2bd393b50b7
apalacios02 <allan.palacios28@gmail.com>
2024-07-11 15:46:03 -0500

    created index main page and controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
     def index
-      @categories = Category.all # Example: Fetch all categories
-      @featured_products = Product.featured.limit(4) # Example: Fetch featured products
+      # @categories = Category.all # Example: Fetch all categories
+      # @featured_products = Product.featured.limit(4) # Example: Fetch featured products
     end
   
     def about
