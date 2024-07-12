1e49fafda77e79e7146a36496601671f1af0c458
apalacios02 <allan.palacios28@gmail.com>
2024-07-11 19:12:32 -0500

    Added active storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
-# product.rb
+# app/models/product.rb
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
+
+  # Active Storage association for product image
+  has_one_attached :image
+
+  # Validation for image attachment
+  validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
